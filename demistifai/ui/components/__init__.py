b25ead37d20bacd06d335721b4ea6e50043ec370
ImSingingInTheRain <limo.escape.0i@icloud.com>
2025-10-18 10:58:59 +0200

    Extract intro hero component

--- demistifai/ui/components/__init__.py
+++ demistifai/ui/components/__init__.py
@@ -7,6 +7,13 @@ from .arch_demai import (
     render_demai_architecture,
 )
 from .control_room import stage_control_room
+from .intro_hero import (
+    intro_ai_act_quote_wrapper_close,
+    intro_ai_act_quote_wrapper_open,
+    intro_hero_scoped_css,
+    intro_lifecycle_columns,
+    render_intro_hero,
+)
 from .mac_window import render_mac_window
 from .train_animation import (
     build_training_animation_column,
@@ -19,7 +26,12 @@ __all__ = [
     "demai_architecture_markup",
     "demai_architecture_styles",
     "render_demai_architecture",
+    "intro_ai_act_quote_wrapper_close",
+    "intro_ai_act_quote_wrapper_open",
+    "intro_hero_scoped_css",
+    "intro_lifecycle_columns",
     "render_mac_window",
+    "render_intro_hero",
     "build_training_animation_column",
     "render_training_animation",
     "stage_control_room",
